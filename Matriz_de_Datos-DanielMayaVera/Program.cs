ab6d6270a5155749e587915464227d63fccd9fbd
danielmayav <60054217+danielmayav@users.noreply.github.com>
2022-05-25 22:42:56 -0500

    Add project files.

---
+++ Matriz_de_Datos-DanielMayaVera/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matriz_de_Datos_DanielMayaVera
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMatrices());
+        }
+    }
+}
