0c4d1d85caf298b6c6abc829698db3914e3bd629
danielmayav <60054217+danielmayav@users.noreply.github.com>
2022-05-26 22:18:45 -0500

    done

--- Matriz_de_Datos-DanielMayaVera/Form1.Designer.cs
+++ Matriz_de_Datos-DanielMayaVera/Form1.Designer.cs
@@ -28,22 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvMatriz = new System.Windows.Forms.DataGridView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MATRIZ DE DATOS";
+            // 
+            // dgvMatriz
+            // 
+            this.dgvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz.Location = new System.Drawing.Point(61, 92);
+            this.dgvMatriz.Name = "dgvMatriz";
+            this.dgvMatriz.ReadOnly = true;
+            this.dgvMatriz.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMatriz.Size = new System.Drawing.Size(531, 179);
+            this.dgvMatriz.TabIndex = 1;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(247, 335);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(131, 53);
+            this.btnCargar.TabIndex = 2;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
             // frmMatrices
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.dgvMatriz);
+            this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMatrices";
             this.Text = "Matriz de Datos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvMatriz;
+        private System.Windows.Forms.Button btnCargar;
     }
 }
 
--- Matriz_de_Datos-DanielMayaVera/Form1.cs
+++ Matriz_de_Datos-DanielMayaVera/Form1.cs
@@ -16,5 +16,44 @@ namespace Matriz_de_Datos_DanielMayaVera
         {
             InitializeComponent();
         }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            string[,] personas = new string[10, 4]
+            {
+                {"123456", "Pablo", "Rodriguez", "24" },
+                {"124564", "Sandra", "Perez", "35" },
+                {"164658", "Pedro", "Sanchez", "44" },
+                {"132654", "Juan", "Vera", "23" },
+                {"111265", "Santiago", "Restrepo", "52" },
+                {"964421", "Esteban", "Moreno", "19" },
+                {"321654", "Luisa", "Cardona", "36" },
+                {"795463", "Paula", "Ospina", "38" },
+                {"579564", "Carolina", "Uribe", "45" },
+                {"223546", "Carlos", "Botero", "32" }
+            };
+
+            int fila = 10;
+            int column = 4;
+
+            dgvMatriz.ColumnCount = column;
+            dgvMatriz.RowCount = fila;
+
+            dgvMatriz.Columns[0].HeaderText = "CEDULA";
+            dgvMatriz.Columns[1].HeaderText = "NOMBRE";
+            dgvMatriz.Columns[2].HeaderText = "APELLIDO";
+            dgvMatriz.Columns[3].HeaderText = "EDAD";
+
+            for(int i = 0; i < fila - 1; i++)
+            {
+                for(int j = 0; j < column - 1; j++)
+                {
+                    dgvMatriz.Rows[i].Cells[j].Value = personas[i,j].ToString();
+                }
+            }
+
+
+        }
+
     }
 }
